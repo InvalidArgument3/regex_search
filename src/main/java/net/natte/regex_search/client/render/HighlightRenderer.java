0e6dbc111c3b23dd8a07ce055809910e18d543d4
InvalidArgument3 <InvalidArgument3@gmail.com>
2026-04-20 02:08:31 -0500

    functional

--- src/main/java/net/natte/regex_search/client/render/HighlightRenderer.java
+++ src/main/java/net/natte/regex_search/client/render/HighlightRenderer.java
@@ -27,7 +27,6 @@ import java.util.List;
 
 
 public class HighlightRenderer {
-
     private static final ResourceLocation ARROW_TEXTURE = RegexSearch.ID("textures/arrow.png");
     private static List<MarkedInventory> inventories = new ArrayList<>();
 
@@ -132,9 +131,7 @@ public class HighlightRenderer {
     }
 
     private static void processItems(PoseStack matrixStack, Matrix4f projectionMatrix, Camera camera, float tickDelta) {
-
-        var modelViewMatrix = matrixStack.last().pose();
-
+        Matrix4f modelViewMatrix = matrixStack.last().pose();
         for (RenderedItem renderedItem : getRenderedItems()) {
             renderedItem.screenPosition = project3Dto2D(renderedItem, modelViewMatrix, projectionMatrix, camera);
         }
@@ -201,25 +198,20 @@ public class HighlightRenderer {
     }
 
     public static Vector4f project3Dto2D(RenderedItem renderedItem, Matrix4f modelViewMatrix, Matrix4f projectionMatrix, Camera camera) {
-
-        Vec3 cameraPosition = camera.getPosition();
-        Quaternionf cameraRotation = camera.rotation();
-        Vec3 in3d = renderedItem.position.subtract(cameraPosition);
-
         var wnd = Minecraft.getInstance().getWindow();
-        var quaternion = new Quaternionf((float) in3d.x, (float) in3d.y, (float) in3d.z, 1.f);
-
-        Matrix4f m = modelViewMatrix.rotate(cameraRotation.invert()); // this
-        var product = mqProduct(projectionMatrix, mqProduct(m, quaternion));
-        modelViewMatrix.rotate(cameraRotation); // undo that
+        Vec3 in3d = renderedItem.position.subtract(camera.getPosition());
+        Quaternionf quaternion = new Quaternionf((float) in3d.x, (float) in3d.y, (float) in3d.z, 1f);
+        Matrix4f m = new Matrix4f(modelViewMatrix);
+        m.rotate(new Quaternionf(camera.rotation()).invert());
+        Quaternionf product = mqProduct(projectionMatrix, mqProduct(m, quaternion));
 
         if (product.w <= 0f) {
             return null;
         }
 
-        var screenPos = qToScreen(product);
-        var x = screenPos.x * wnd.getWidth();
-        var y = screenPos.y * wnd.getHeight();
+        Quaternionf screenPos = qToScreen(product);
+        float x = screenPos.x * wnd.getWidth();
+        float y = screenPos.y * wnd.getHeight();
 
         if (Float.isInfinite(x) || Float.isInfinite(y)) {
             return null;
@@ -229,25 +221,20 @@ public class HighlightRenderer {
     }
 
     public static Vec2 project3Dto2D(Vec3 worldPosition, Matrix4f modelViewMatrix, Matrix4f projectionMatrix, Camera camera) {
-
-        Vec3 cameraPosition = camera.getPosition();
-        Quaternionf cameraRotation = camera.rotation();
-        Vec3 in3d = worldPosition.subtract(cameraPosition);
-
         var wnd = Minecraft.getInstance().getWindow();
-        var quaternion = new Quaternionf((float) in3d.x, (float) in3d.y, (float) in3d.z, 1.f);
-
-        Matrix4f m = modelViewMatrix.rotate(cameraRotation.invert()); // this
-        var product = mqProduct(projectionMatrix, mqProduct(m, quaternion));
-        modelViewMatrix.rotate(cameraRotation); // undo that
+        Vec3 in3d = worldPosition.subtract(camera.getPosition());
+        Quaternionf quaternion = new Quaternionf((float) in3d.x, (float) in3d.y, (float) in3d.z, 1f);
+        Matrix4f m = new Matrix4f(modelViewMatrix);
+        m.rotate(new Quaternionf(camera.rotation()).invert());
+        Quaternionf product = mqProduct(projectionMatrix, mqProduct(m, quaternion));
 
         if (product.w <= 0f) {
             return null;
         }
 
-        var screenPos = qToScreen(product);
-        var x = screenPos.x * wnd.getWidth();
-        var y = screenPos.y * wnd.getHeight();
+        Quaternionf screenPos = qToScreen(product);
+        float x = screenPos.x * wnd.getWidth();
+        float y = screenPos.y * wnd.getHeight();
 
         if (Float.isInfinite(x) || Float.isInfinite(y)) {
             return null;
@@ -265,13 +252,8 @@ public class HighlightRenderer {
     }
 
     private static Quaternionf qToScreen(Quaternionf q) {
-        var w = 1f / q.w * 0.5f;
-
-        return new Quaternionf(
-                q.x * w + 0.5f,
-                q.y * w + 0.5f,
-                q.z * w + 0.5f,
-                w);
+        float w = 1f / q.w * 0.5f;
+        return new Quaternionf(q.x * w + 0.5f, q.y * w + 0.5f, q.z * w + 0.5f, w);
     }
 
     public static void renderGuiItemModel(ItemStack itemStack, double x, double y, BakedModel model, float scale, RenderedItem renderedItem) {
